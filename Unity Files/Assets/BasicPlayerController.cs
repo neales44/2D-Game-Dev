2c9d12aba332aae68499bdc6bcb8e2a86a0bd4b5
vixicat <dustyisntsorusty@gmail.com>
2025-11-04 21:28:50 -0800

    fixed space hold implementation

--- Unity Files/Assets/BasicPlayerController.cs
+++ Unity Files/Assets/BasicPlayerController.cs
@@ -125,7 +125,7 @@ public class BasicPlayerController : MonoBehaviour
         if (downTimer > 0) {
             if (rb.linearVelocity.y > 0) {
                 downTimer -= Time.deltaTime;
-                var downVec = new Vector2(0, -(downForce * Time.deltaTime));
+                var downVec = new Vector2(0, downForce * Time.deltaTime);
                 rb.linearVelocity = rb.linearVelocity - downVec;
             } else {
                 downTimer = 0;
