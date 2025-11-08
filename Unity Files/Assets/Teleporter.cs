fcd6aa3bb611401287d4a13a802beb72fd04e3b9
Austin Christian <austin2003christian@gmail.com>
2025-11-07 18:22:57 -0800

    added my features

---
+++ Unity Files/Assets/Teleporter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform GetDestination()
+    {
+        return destination;
+    }
+    
+}
